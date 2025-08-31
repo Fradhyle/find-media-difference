17968a2ffec45a14df6864bd66b9e22a76e86bef
Donghyeok Im <fradhyle@gmail.com>
2025-08-31 13:46:29 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+
+def count_ext(path: Path) -> dict[str, int]:
+    """Count the number of files for each file extension in the given directory and its subdirectories.
+
+    Args:
+        path (Path): Path to search.
+
+    Returns:
+        dict[str, int]: A dictionary with file extensions as keys and their counts as values.
+    """
+    ext_count = {}
+
+    for file in path.rglob("*"):
+        if file.is_file():
+            ext = file.suffix.lower()
+            if ext in ext_count:
+                ext_count[ext] += 1
+            else:
+                ext_count[ext] = 1
+
+    return ext_count
+
+
+def compare_files(path1: Path, path2: Path) -> list[Path]:
+    """Compare files in two directories and find files with the same name and returns name of files not exists in both directory.
+
+    Args:
+        path1 (Path): The first directory to compare.
+        path2 (Path): The second directory to compare.
+
+    Returns:
+        list[Path]: A list of files that are unique to either directory."""
+    files1 = {file.relative_to(path1) for file in path1.rglob("*") if file.is_file()}
+    files2 = {file.relative_to(path2) for file in path2.rglob("*") if file.is_file()}
+
+    unique = files1.symmetric_difference(files2)
+
+    unique_files = []
+
+    for rel_path in unique:
+        if rel_path in files1:
+            unique_files.append(path1 / rel_path)
+        elif rel_path in files2:
+            unique_files.append(path2 / rel_path)
+
+    return unique_files
+
+
+if __name__ == "__main__":
+    first_path = Path(r"input().strip("\u202a")")
+    second_path = Path(r"input().strip("\u202a")")
+    print("First path:", count_ext(first_path))
+    print("Second path:", count_ext(second_path))
+
+    with open("unique_files.txt", "a+") as f:
+        for file in compare_files(first_path, second_path):
+            f.write(str(file) + "\n")
